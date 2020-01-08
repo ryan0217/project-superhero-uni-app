b7f9f513d0cfb04342a9f868a9638a71e5867827
孙瑞 <183428323@qq.com>
2020-01-08 09:48:47 +0800

    实现首页热门超英板块的基本展示

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -13,6 +13,34 @@
       </swiper-item>
     </swiper>
     <!-- 轮播图 end -->
+
+    <!-- 热门超英 start -->
+    <view class="page-block hot-super-container">
+      <view class="title-container">
+        <image class="title-icon" src="../../static/images/index/icon-hot-super.png"></image>
+        <view class="title-text">热门超英</view>
+      </view>
+      <scroll-view class="hot-super-scroll-container" scroll-x="true">
+        <view class="hot-super-scroll-item" v-for="item in hotMovieList" :key="item.id">
+          <image class="poster" :src="item.cover"></image>
+          <view class="title">{{item.name}}</view>
+          <view class="score">
+            <image
+              class="icon-star"
+              v-for="(sItem, sIndex) in Math.floor(item.score / 2)"
+              :key="'yellow-' + sIndex"
+              src="../../static/images/index/star-yellow.png"></image>
+            <image
+              class="icon-star"
+              v-for="(sItem, sIndex) in (5 - Math.floor(item.score / 2))"
+              :key="'gray-' + sIndex"
+              src="../../static/images/index/star-gray.png"></image>
+            {{item.score}}
+          </view>
+        </view>
+      </scroll-view>
+    </view>
+    <!-- 热门超英 end -->
   </view>
 </template>
 
@@ -22,12 +50,14 @@
   export default {
     data() {
       return {
-        carouselList: []
+        carouselList: [],
+        hotMovieList: []
       }
     },
     async onLoad () {
       await getQQ()
       this.getCarouselList()
+      this.getHotMovies()
     },
     methods: {
       getCarouselList() {
@@ -35,6 +65,16 @@
           url: "/index/carousel/list",
           success: data => {
             this.carouselList = data
+          }
+        })
+      },
+      getHotMovies() {
+        post({
+          url: "/index/movie/hot",
+          data: { type: "superhero" },
+          success: data => {
+            console.log("getHotMovies", data)
+            this.hotMovieList = data
           }
         })
       }
@@ -50,5 +90,76 @@
   .swiper-item {
     width: 100%;
     height: 100%;
+  }
+
+  .page-block {
+    margin-top: 12upx;
+    padding: 20upx;
+    background-color: #fff;
+  }
+  .title-container {
+    display: flex;
+    align-items: center;
+    line-height: 52upx;
+
+    .title-icon {
+      width: 30upx;
+      height: 30upx;
+    }
+    .title-text {
+      margin-left: 20upx;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+
+  .hot-super-container {
+    padding: 20upx 0 2upx;
+
+    .title-container {
+      padding-right: 20upx;
+      padding-left: 20upx;
+    }
+  }
+  .hot-super-scroll-container {
+    margin-top: 20upx;
+    white-space: nowrap;
+  }
+  .hot-super-scroll-item {
+    display: inline-block;
+    margin-left: 20upx;
+    margin-bottom: 18upx;
+    width: 200upx;
+
+    &:last-child {
+      margin-right: 20upx;
+    }
+    .poster {
+      width: 100%;
+      height: 270upx;
+    }
+    .title {
+      width: 100%;
+      font-size: 14px;
+      font-weight: bold;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .score {
+      display: flex;
+      align-items: center;
+      height: 32upx;
+      font-size: 12px;
+      color: grey;
+    }
+    .icon-star {
+      width: 20upx;
+      height: 20upx;
+
+      &:last-child {
+        margin-right: 8upx;
+      }
+    }
   }
 </style>
